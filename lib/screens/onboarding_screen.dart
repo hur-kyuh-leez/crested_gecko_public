2e5d45de6aa8be82b010547995d41aea2e5ac408
hur-kyuh-leez <hyukkyulee@alumni.emory.edu>
2022-05-24 23:59:12 +0900

    onboarding

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -28,7 +28,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             controller: _controller,
             onPageChanged: (index) {
               setState(() {
-                onLastPage = (index == 2);
+                onLastPage = (index == 6);
                 onFirstPage = (index == 0); // return true if index is 2
               });
             },
@@ -59,10 +59,10 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                       Padding(
                         padding: const EdgeInsets.only(top: 100.0),
                         child: Lottie.network(
-                            'https://assets3.lottiefiles.com/private_files/lf30_ZenthI.json'),
+                            'https://assets6.lottiefiles.com/packages/lf20_4dg6ijc2.json'),
                       ),
                       Text(
-                        '"중간 유통을 줄이면 될 거 같은데..."',
+                        '"구글링...카페글...카페분들의 조언을 들어보니..."',
                         style: TextStyle(color: Colors.white),
                       ),
                     ],
@@ -80,8 +80,110 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                             'https://assets3.lottiefiles.com/private_files/lf30_ZenthI.json'),
                       ),
                       Text(
-                        '"중간 유통을 줄이면 될 거 같은데..."',
+                        '"중간 유통을 줄이면 될 거 같은데...?!"',
                         style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.black,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 100.0),
+                        child: Lottie.network(
+                            'https://assets5.lottiefiles.com/packages/lf20_89tq6c8d.json'),
+                      ),
+                      SizedBox(height: 70),
+                      Text(
+                        '"직접 해외에서 데려오자!"',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.black,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
+                        child: Lottie.network(
+                            'https://assets6.lottiefiles.com/packages/lf20_zJSuaQ.json'),
+                      ),
+                      SizedBox(height: 70),
+                      Text(
+                        '"방식은 공정하게 래플(응모)!"',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.black,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30),
+                        child: SizedBox(
+                          height: 250,
+                          child: Lottie.network(
+                              'https://assets7.lottiefiles.com/datafiles/40aX5db74VvGPWw/data.json'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: Text(
+                          '(아직 진행하고 있지 않아요! 혼자 계획중입니다!\n'
+                          '허락 아직 안받았어요!)\n'
+                          '  \n'
+                          '참여 자격: \n'
+                          '1. 크레와 사랑에 빠지신 분들\n'
+                          '2. 파사모 회원 이면서 1개월 이상 활동중 이신 분들!\n'
+                          '   \n'
+                          '확률 가산점: 도움 주시는 모든 분들!\n',
+                          style: TextStyle(
+                            height: 2,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.black,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
+                        child: Container(
+                          height: 200,
+                          child: Lottie.network(
+                              'https://assets4.lottiefiles.com/packages/lf20_buhby0ug.json'),
+                        ),
+                      ),
+                      Text(
+                        '이런 건/분 안되요! \n',
+                        style: TextStyle(
+                            fontSize: 30, height: 1.5, color: Colors.white),
+                      ),
+                      Text(
+                        '1. 중복 응모\n'
+                        '2. 되팔이\n'
+                        '3. 분쟁 글 있는 회원\n'
+                        '4. 고택 (죄송합니다...)\n'
+                        '...',
+                        style: TextStyle(height: 1.5, color: Colors.white),
                       ),
                     ],
                   ),
@@ -115,7 +217,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                         ),
 
                   // dot indicators
-                  SmoothPageIndicator(controller: _controller, count: 3),
+                  SmoothPageIndicator(controller: _controller, count: 7),
 
                   // next or done
                   onLastPage
